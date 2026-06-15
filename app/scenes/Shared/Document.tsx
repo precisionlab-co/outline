b21379eba7c2ddfe0bd2b6d70ad02033a355e0fb
Camilo Sevilla <acsevillam@gmail.com>
2026-06-15 18:39:49 -0500

    fix: remove Outline branding button from public share pages

--- app/scenes/Shared/Document.tsx
+++ app/scenes/Shared/Document.tsx
@@ -4,13 +4,10 @@ import type { PublicTeam } from "@shared/types";
 import { TOCPosition } from "@shared/types";
 import type DocumentModel from "~/models/Document";
 import DocumentComponent from "~/scenes/Document/components/Document";
-import Branding from "~/components/Branding";
 import { useDocumentContext } from "~/components/DocumentContext";
 import { useTeamContext } from "~/components/TeamContext";
-import useCurrentUser from "~/hooks/useCurrentUser";
 import useQuery from "~/hooks/useQuery";
 import useShare from "@shared/hooks/useShare";
-import { parseDomain } from "@shared/utils/domains";
 
 type Props = {
   document: DocumentModel;
@@ -21,15 +18,9 @@ function SharedDocument({ document }: Props) {
   const query = useQuery();
   const searchTerm = query.get("q") || undefined;
   const team = useTeamContext() as PublicTeam | undefined;
-  const user = useCurrentUser({ rejectOnEmpty: false });
   const { hasHeadings, setDocument, isEditorInitialized, editor } =
     useDocumentContext();
   const abilities = useMemo(() => ({}), []);
-  const isCustomDomain = useMemo(
-    () => parseDomain(window.location.origin).custom,
-    []
-  );
-  const showBranding = !isCustomDomain && !user;
   const searchTermProcessed = useRef<string | null>(null);
 
   const tocPosition = hasHeadings
@@ -59,9 +50,6 @@ function SharedDocument({ document }: Props) {
         tocPosition={tocPosition}
         readOnly
       />
-      {showBranding ? (
-        <Branding href="//www.getoutline.com?ref=sharelink" />
-      ) : null}
     </>
   );
 }
